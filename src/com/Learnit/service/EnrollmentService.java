1261e6d008d19bbdc2cae8fa4371caf8eb8872fb
Davo-lol <arguetavallesdavid@gmail.com>
2026-03-25 19:07:17 -0600

    n

--- src/com/Learnit/service/EnrollmentService.java
+++ src/com/Learnit/service/EnrollmentService.java
@@ -14,6 +14,14 @@ public class EnrollmentService {
         enrollments.add(enrollment);
         System.out.println("Inscripción realizada correctamente :)");
     }
+    public void actualizarProgresos( String usuariosEstudiante, String codigoCurse,Double nuevoProgreso){
+        if (nuevoProgreso < 0 || nuevoProgreso > 100){
+            System.out.println("Error: el progreso debe ser un valor entre 0 y 100,");
+            return;
+        }
+        for (Enrollment enrollment : enrollments){
+            boolean mismoEstudiante = enrollment.getEstudiante().getUsuario().equals(usuariosEstudiante);
+            boolean mismoCuros = enrollment.getCurso().getCodigoCurso().equals(codigoCurse);
 
 
     public ArrayList<Enrollment> getEnrollments() {
